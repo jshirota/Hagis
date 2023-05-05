96ad0b977d28583e022aad00ea1bdfbbc93bfcd1
Jiro Shirota <jiro.shirota@innovapost.com>
2023-05-05 14:39:47 -0400

    .

--- hagis.py
+++ hagis.py
@@ -318,7 +318,7 @@ class Layer(Generic[T]):  # pylint: disable=too-many-instance-attributes
             if date_fields:
                 for key, value in feature.attributes.__dict__.items():
                     if key in date_fields and value:
-                        feature.attributes.__dict__[key] = datetime.fromtimestamp(value / 1000)
+                        feature.attributes.__dict__[key] = datetime.utcfromtimestamp(value / 1000)
             if hasattr(feature, "geometry") and feature.geometry and hasattr(obj, "spatialReference"):
                 feature.geometry.spatialReference = obj.spatialReference.__dict__
 
@@ -368,23 +368,22 @@ class Layer(Generic[T]):  # pylint: disable=too-many-instance-attributes
                         yield self._map(row)
 
     def _to_sql(self, predicate: Callable[[T], bool]) -> str:
-
         class LambdaFinder(ast.NodeVisitor):
             def __init__(self, expression: Any) -> None:
                 super().__init__()
 
-                # Capture closure variables.
-                closure = expression.__closure__
-                if closure:
-                    self.freevars = dict(zip(expression.__code__.co_freevars, [x.cell_contents for x in closure]))
-                else:
-                    self.freevars = {}
+                self.freevars: Dict[str, Any] = {}
 
                 # Check globals.
                 for name in expression.__code__.co_names:
                     if name in expression.__globals__:
-                        if name not in self.freevars:
-                            self.freevars[name] = expression.__globals__[name]
+                        self.freevars[name] = expression.__globals__[name]
+
+                # Capture closure variables.
+                closure = expression.__closure__
+                if closure:
+                    for name, value in zip(expression.__code__.co_freevars, [x.cell_contents for x in closure]):
+                        self.freevars[name] = value
 
                 line = getsource(expression).strip()
                 self.visit(ast.parse(f"{line}\n    pass" if line.endswith(":") else line))
@@ -398,8 +397,7 @@ class Layer(Generic[T]):  # pylint: disable=too-many-instance-attributes
                 return visitor.expression, visitor.freevars
 
         class LambdaVisitor(ast.NodeVisitor):
-
-            def __init__(self, expression: ast.expr, freevars: Dict[str, Any], fields: Dict[str, str]):
+            def __init__(self, expression: ast.expr, freevars: Dict[str, Any], fields: Dict[str, str]) -> None:
                 super().__init__()
                 self._expressions: List[Union[LambdaVisitor, str]] = []
                 self._freevars = freevars
@@ -407,8 +405,12 @@ class Layer(Generic[T]):  # pylint: disable=too-many-instance-attributes
                 self.visit(expression)
 
             def visit_Attribute(self, node: Attribute) -> Any:
-                field_name = self._fields[node.attr]
-                self._expressions.append(field_name)
+                attr = node.attr
+                value: Any = node.value
+                if value.id in self._freevars:
+                    self._expressions.append(self._get_sql_value(getattr(self._freevars[value.id], attr)))
+                else:
+                    self._expressions.append(self._fields[attr])
 
             def visit_BoolOp(self, node: BoolOp) -> Any:
                 self._expressions.append("(")
@@ -443,13 +445,12 @@ class Layer(Generic[T]):  # pylint: disable=too-many-instance-attributes
                     self._expressions.append(LambdaVisitor(node.comparators[0], self._freevars, self._fields))
 
             def visit_Constant(self, node: Constant) -> Any:
-                self._expressions.append(self._get_sql_value(node))
+                self._expressions.append(self._get_sql_value(node.value))
 
             def visit_Name(self, node: Name) -> Any:
-                self._expressions.append(self._get_sql_value(node))
+                self._expressions.append(self._get_sql_value(self._freevars[node.id]))
 
-            def _get_sql_value(self, node: Any) -> str:
-                value = self._get_value(node)
+            def _get_sql_value(self, value: Any) -> str:
                 if value is None:
                     return "NULL"
                 if isinstance(value, str):
@@ -490,7 +491,10 @@ class Layer(Generic[T]):  # pylint: disable=too-many-instance-attributes
                     text += e.to_sql() if isinstance(e, LambdaVisitor) else f" {e}"
                 return text
 
+        # Find the lambda expression and any free variables encapsulated in it.
         expression, freevars = LambdaFinder.find(predicate)
+
+        # Generate a where clause.
         where_clause = LambdaVisitor(expression, freevars, self._fields).to_sql().strip()
 
         return where_clause
